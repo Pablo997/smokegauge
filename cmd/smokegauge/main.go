7696009d07a9db332363de5ac28bd7786957191f
Pablo Aragonés <pablo.aragones@3xs.es>
2026-05-12 11:31:27 +0200

    Add error handling for config file reading in smokegauge tool and introduce a sample configuration file for testing

--- cmd/smokegauge/main.go
+++ cmd/smokegauge/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
 )
 
+// Exit status 2: invalid CLI usage or unreadable config file. Failed HTTP checks will use 1.
+
+func check(path string, err error) {
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			fmt.Fprintf(os.Stderr, "smokegauge: config file does not exist: %s\n", path)
+		} else {
+			fmt.Fprintf(os.Stderr, "smokegauge: cannot read config file: %s: %s\n", path, err)
+		}
+		os.Exit(2)
+	}
+}
+
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintln(os.Stderr, "usage: smokegauge -file <path>")
+		fmt.Fprintln(os.Stderr, "smokegauge: usage: smokegauge -file <path>")
 		flag.PrintDefaults()
 	}
 
@@ -17,9 +32,10 @@ func main() {
 
 	if *fileName == "" {
 		fmt.Fprintln(os.Stderr, "smokegauge: required flag -file not provided")
-		fmt.Fprintln(os.Stderr, "usage: smokegauge -file <path>")
+		fmt.Fprintln(os.Stderr, "smokegauge: usage: smokegauge -file <path>")
 		os.Exit(2)
 	}
 
-	fmt.Println(*fileName)
+	_, err := os.ReadFile(*fileName)
+	check(*fileName, err)
 }
